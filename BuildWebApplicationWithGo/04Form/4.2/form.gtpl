c75044f1955b1b323d9462fde2c520903d7dcbb3
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2020-12-22 20:32:04 +0900

    Add BWAWG 04 Form 4.2

---
+++ BuildWebApplicationWithGo/04Form/4.2/form.gtpl
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form action="/login?username=kitoria" method="post">
+    ユーザ名:<input type="text" name="username">
+    パスワード:<input type="password" name="password">
+    <br><br>
+    <select name="fruit">
+        <option value="apple">apple</option>
+        <option value="pear">pear</option>
+        <option value="banane">banane</option>
+    </select>
+    <br><br>
+
+    <input type = "radio" name = "gender" value="1">男
+    <input type = "radio" name = "gender" value="2">女
+    <br><br>
+    
+    <input type="checkbox" name="interest" value="football">サッカー
+    <input type="checkbox" name="interest" value="basketball">バスケットボール
+    <input type="checkbox" name="interest" value="tennis">テニス
+    
+    <br><br>
+    <input type="submit" value="提出">
+</form>
+</body>
+</html>
---
+++ BuildWebApplicationWithGo/04Form/4.2/form_verify.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello kitoria")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("form.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+		if isFruit(r) {
+			fmt.Println("button", r.Form["fruit"])
+		} else {
+			fmt.Fprintf(w, "Error")
+		}
+		fmt.Println("Gender", r.Form["gender"])
+		fmt.Println("Gender", r.Form["interest"])
+
+	}
+}
+
+func isFruit(r *http.Request) bool {
+	slice := []string{"apple", "pear", "banane"}
+	for _, v := range slice {
+		if v == r.Form.Get("fruit") {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
