efae820cf66d9bcde2c5dafce12f4e1adec51865
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-02-25 22:59:20 +0900

    Add GoWebProg Chap03

---
+++ GoWebProgramming/Chap03/list3-8.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
+func world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
+
+	fmt.Println(reflect.TypeOf(hello))
+
+	server.ListenAndServe()
+}
